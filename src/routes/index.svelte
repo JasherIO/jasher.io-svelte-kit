7bb88f0f3d486eaa78571deb3dca9e609925cf39
Joshua C. Willis <3475947+JasherIO@users.noreply.github.com>
2022-03-06 21:41:10 -0600

    main for semantics

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,7 @@
-<script>
+<script lang="ts">
   import Hero from "$lib/components/Hero.svelte";
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<Hero />
+<main>
+  <Hero />
+</main>
