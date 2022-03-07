15d9e2be787bdfda551a4066e07ca38beef7a258
Joshua C. Willis <3475947+JasherIO@users.noreply.github.com>
2022-03-06 23:50:29 -0600

    prettier responsive contact button; mobile padding for description

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -24,11 +24,11 @@
     <h1 class="max-w-lg mx-auto text-center text-white text-4xl font-extrabold font-display tracking-tight sm:text-5xl lg:text-6xl">
       Hi, I'm Josh!
     </h1>
-    <p class="mt-4 max-w-lg mx-auto text-center text-xl text-green-200 sm:max-w-3xl">
+    <p class="mt-4 px-4 max-w-lg mx-auto text-center text-lg sm:text-xl text-green-200 sm:max-w-3xl">
       A web developer, tournament operator, and soccar enthusiast.
     </p>
     <div class="mt-6 sm:mt-8 mx-auto flex justify-center">
-      <a sveltekit:prefetch href="/contact" class="px-6 sm:px-8 py-2 sm:py-3 text-lg font-bold rounded-full shadow-sm text-green-900 bg-neutral-100 hover:bg-green-50">
+      <a sveltekit:prefetch href="/contact" class="px-6 sm:px-7 py-2 sm:py-2.5 text-base sm:text-lg font-base font-medium rounded-full shadow-sm text-green-900 bg-neutral-100 hover:bg-green-50">
         Contact
       </a>
     </div>
