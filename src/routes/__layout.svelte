ebb4e8c509ee9cc7284efc52e3981d8de3eb0b46
Joshua C. Willis <3475947+JasherIO@users.noreply.github.com>
2022-03-07 07:56:25 -0600

    Google Search Console verification

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -12,17 +12,24 @@
 	<meta name="description" content="A web developer, tournament operator, and soccar enthusiast.">
 	<meta name="keywords" content="Jasher, Portfolio, Software, Web Developer, Esports, Rocket League, Tournament, Referee">
 	<meta name="theme-color" content="#22c55e">
+
+	<!-- Twitter -->
 	<meta name="twitter:card" content="summary">
 	<meta name="twitter:creator" content="JasherIO">
 	<meta name="twitter:description" content="A web developer, tournament operator, and soccar enthusiast.">
 	<!-- <meta name="twitter:image" content="/Jasher.svg"> -->
 	<meta name="twitter:site" content="JasherIO">
 	<meta name="twitter:title" content="Jasher">
+
+	<!-- OpenGraph (Facebook) -->
 	<meta property="og:description" content="A web developer, tournament operator, and soccar enthusiast.">
 	<!-- <meta property="og:image" content="/Jasher.svg"> -->
 	<meta property="og:site_name" content="Jasher">
 	<meta property="og:title" content="Jasher">
 	<meta property="og:type" content="website">
+	
+	<!-- Google Search Console -->
+	<meta name="google-site-verification" content="7Mj0lv4nimdAbagoQ3UhfICE_FRtiQci0-6_NksquJQ" />
 </svelte:head>
 
 <Navbar />
