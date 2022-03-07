2004d1a3fc95328b4c462b16f5f1c6c9e18df0d4
Joshua C. Willis <3475947+JasherIO@users.noreply.github.com>
2022-03-06 22:50:08 -0600

    SEO

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -9,7 +9,20 @@
 
 <svelte:head>
 	<title>Jasher</title>
-	<meta name="description" content="A web developer, tournament operator, and soccar enthusiast." />
+	<meta name="description" content="A web developer, tournament operator, and soccar enthusiast.">
+	<meta name="keywords" content="Jasher, Portfolio, Software, Web Developer, Esports, Rocket League, Tournament, Referee">
+	<meta name="theme-color" content="#22c55e">
+	<meta name="twitter:card" content="summary">
+	<meta name="twitter:creator" content="JasherIO">
+	<meta name="twitter:description" content="A web developer, tournament operator, and soccar enthusiast.">
+	<!-- <meta name="twitter:image" content="/Jasher.svg"> -->
+	<meta name="twitter:site" content="JasherIO">
+	<meta name="twitter:title" content="Jasher">
+	<meta property="og:description" content="A web developer, tournament operator, and soccar enthusiast.">
+	<!-- <meta property="og:image" content="/Jasher.svg"> -->
+	<meta property="og:site_name" content="Jasher">
+	<meta property="og:title" content="Jasher">
+	<meta property="og:type" content="website">
 </svelte:head>
 
 <Navbar />
