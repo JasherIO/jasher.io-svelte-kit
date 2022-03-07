a147c04e9cc8de99106a296ca84d19dcaf3c90e7
Joshua C. Willis <3475947+JasherIO@users.noreply.github.com>
2022-03-07 11:15:59 -0600

    fixed fetch listener

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -13,18 +13,17 @@ self.addEventListener('install', (event) => {
 
 // https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Offline_Service_workers#responding_to_fetches
 self.addEventListener('fetch', (event) => {
-  event.respondWith(async () => {
+  event.respondWith((async () => {
     const cache_response = await caches.match(event.request);
-    
     if (cache_response)
       return cache_response;
 
     const fetch_response = await fetch(event.request);
     const cache = await caches.open(cache_name);
-    cache.put(event.request.url, fetch_response.clone());
+    cache.put(event.request, fetch_response.clone());
     
     return fetch_response;
-  });
+  })());
 });
 
 const clean = (key) => {
