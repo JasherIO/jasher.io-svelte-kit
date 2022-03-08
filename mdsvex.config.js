e66a8873752c61328d9c7cdbc37d680ea93a92e5
Joshua C. Willis <3475947+JasherIO@users.noreply.github.com>
2022-03-08 10:20:23 -0600

    default layout for markdown blog posts

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,7 +1,13 @@
 import { defineMDSveXConfig as defineConfig } from 'mdsvex';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const config = defineConfig({
 	extensions: ['.svelte.md', '.md', '.svx'],
+
+	layout: path.join(dirname, './src/lib/components/Layouts/blog/[slug].svelte'),
 
 	smartypants: {
 		dashes: 'oldschool'
---
+++ src/lib/components/Layouts/blog/[slug].svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import Head from '$lib/components/Head.svelte';
+  import Container from '$lib/components/Container.svelte';
+  import { to_date } from '$lib/types/post';
+
+  export let title;
+  export let description;
+  export let date;
+  export let category;
+  export let keywords;
+
+  const _date = to_date(date);
+</script>
+
+<Head title={`Jasher | ${title}`} {description} {keywords} />
+
+<Container>
+  <article class="p-2">
+    <span class="text-base md:text-lg text-green-600 dark:text-green-400 font-semibold">{category}</span>
+    <h1 class="text-3xl md:text-5xl font-bold my-1">{title}</h1>
+    <span class="text-base text-neutral-500 font-semibold space-x-2">
+      <!-- <span>{post.stats.text}</span>
+      <span>&bull;</span> -->
+      <time dateTime={_date.ISO}>
+        {_date.text}
+      </time>
+    </span>
+    <div class="max-w-none mx-auto mt-4 selection:bg-green-300 selection:text-green-900 prose md:prose-lg dark:prose-invert prose-neutral prose-a:text-green-600 dark:prose-a:text-green-400 prose-li:my-0.5 md:prose-li:my-0.5 prose-h1:mb-0 md:prose-h1:mb-2">
+      <slot />
+    </div>
+  </article>
+</Container>
